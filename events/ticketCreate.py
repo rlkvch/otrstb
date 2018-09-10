69593da899827461d52ddc146ac7f6319c7243ec
Ilia Arlukevich <arlukevich@protonmail.com>
2018-09-10 15:02:45 +0300

    fix

--- events/ticketCreate.py
+++ events/ticketCreate.py
@@ -5,37 +5,38 @@ import socket
 import sys
 
 import requests
-import pyTelegramBotAPI
+import telebot
 from pyotrs import Article, Client, Ticket
 
 import config
 
-# Debug ################################################################
-# logging.basicConfig(format=u'%(levelname)-8s [%(asctime)s] %(message)s',
-#                     level=logging.DEBUG,
-#                     filename=u'path_to_logs')
-# def debug(*args):
-#     logging.debug(args)
-########################################################################
+# logfile ################################################################
+#  logging.basicConfig(format=u'%(levelname)-8s [%(asctime)s] %(message)s',
+#                      level=logging.DEBUG,
+#                      filename=u'')
+#  def debug(*args):
+#      logging.debug(args)
+##########################################################################
 
 arg1 = sys.argv[1]
 arg2 = sys.argv[2]
 
-def get_ticket():
+def get_ticket(argument):
     client = Client(config.CLIENT_URL, config.USER, config.PASSWD)
     client.session_create()
-    ticket = client.ticket_get_by_number(arg1)
+    ticket = client.ticket_get_by_number(argument,articles=True)
     result = ticket.to_dct()
     json_string = json.dumps(result)
     parsed_string = json.loads(json_string)
     title = (parsed_string["Ticket"]["Title"])
-    user_id = (parsed_string["Ticket"]["CustomerUserID"])
+    user = (parsed_string["Ticket"]["Article"][0]["From"])
     queue = (parsed_string["Ticket"]["Queue"])
+    msg_body = (parsed_string["Ticket"]["Article"][0]["Body"])
     date = (parsed_string["Ticket"]["Created"])
-    phone = (parsed_string["Ticket"]["DynamicField"][0]["Value"])
-    message = (title + '\n' + user_id + '\n' + queue + '\n' + date + '\n' + phone)
+    message = ('Очередь: ' + queue + '\n' + 'От кого: ' + user + '\n' + 'Тема: ' + title + '\n' + msg_body + '\n' + date)
     return message
 
-msg = (get_ticket())
+msg = (get_ticket(arg1))
 bot = telebot.TeleBot(config.BOT_TOKEN)
 bot.send_message(config.CHANNEL_ID, msg)
+
